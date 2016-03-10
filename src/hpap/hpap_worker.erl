503928932630b8105639eed8ae8c40a92dc88f46
xiaotie <wudixiaotie@gmail.com>
2016-03-10 16:38:19 +0800

    update

--- src/hpap/hpap_worker.erl
+++ src/hpap/hpap_worker.erl
@@ -60,3 +60,6 @@ code_change(_OldVer, State, _Extra) -> {ok, State}.
 %% ===================================================================
 %% Internal functions
 %% ===================================================================
+
+migrate_path(PoolName) ->
+    WorkerList = supervisor:which_children(PoolName),
